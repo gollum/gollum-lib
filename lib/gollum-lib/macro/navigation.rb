3fb422a5ac0e2e660d0f853b86e4b42cd82cb1fe
rgroux <richard.groux+github@gmail.com>
2015-12-02 14:41:01 +0100

    request(macro): rename SubGlobalToc to Navigation
    Thanks @dometto who find a better name

--- lib/gollum-lib/macro/navigation.rb
+++ lib/gollum-lib/macro/navigation.rb
@@ -1,10 +1,10 @@
 module Gollum
   class Macro
-    class SubGlobalTOC < Gollum::Macro
-      def render(title = "Sub Global Table of Contents", toc_path = "")
+    class Navigation < Gollum::Macro
+      def render(title = "Navigate in the TOC", toc_root_path = "")
         if @wiki.pages.size > 0
           result = '<ul>' + @wiki.pages.map {|p|
-            if p.url_path.match(toc_path)
+            if p.url_path.match(toc_root_path)
               "<li><a href=\"#{p.url_path}\">#{p.url_path_display}</a></li>"
             end
 	  }.join + '</ul>'
