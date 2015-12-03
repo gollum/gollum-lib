57ccb9ddcfe68caca4872b4eef98a66d0e90e83d
Bart Kamphorst <bart@kamphorst.com>
2015-12-03 23:00:59 +0100

    Fix minor issues with indentation.

--- lib/gollum-lib/macro/navigation.rb
+++ lib/gollum-lib/macro/navigation.rb
@@ -1,7 +1,7 @@
 module Gollum
   class Macro
     class Navigation < Gollum::Macro
-      
+
       def render(title = "Navigate in the TOC", toc_root_path = ::File.dirname(@page.path), full_path = false)
         if @wiki.pages.size > 0
           list_items = @wiki.pages.map do |page|
@@ -9,12 +9,12 @@ module Gollum
               path_display = full_path ? page.url_path_display  : page.url_path_display.sub(toc_root_path,"").sub(/^\//,'')
               "<li><a href=\"/#{page.url_path}\">#{path_display}</a></li>"
             end
-	        end
-	        result = "<ul>#{list_items.join}</ul>"
+          end
+          result = "<ul>#{list_items.join}</ul>"
         end
         "<div class=\"toc\"><div class=\"toc-title\">#{title}</div>#{result}</div>"
       end
-      
+
     end
   end
 end
