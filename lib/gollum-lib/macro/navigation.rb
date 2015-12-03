101e0aa8406996ff70f5c7d59f96a8f99b2de5f7
rgroux <richard.groux+github@gmail.com>
2015-12-03 16:55:54 +0100

    refactor(Navigation): very nice stylistic suggestion from bartkamphorst

--- lib/gollum-lib/macro/navigation.rb
+++ lib/gollum-lib/macro/navigation.rb
@@ -3,12 +3,13 @@ module Gollum
     class Navigation < Gollum::Macro
       def render(title = "Navigate in the TOC", toc_root_path = ::File.dirname(@page.path), full_path = false)
         if @wiki.pages.size > 0
-          result = '<ul>' + @wiki.pages.map {|p|
-            if p.url_path.start_with?(toc_root_path)
-	      path_display = full_path ? p.url_path_display  : p.url_path_display.sub(toc_root_path,"").sub(/^\//,'')
-              "<li><a href=\"/#{p.url_path}\">#{path_display}</a></li>"
+          list_items = @wiki.pages.map do |page|
+            if page.url_path.start_with?(toc_root_path)
+	      path_display = full_path ? page.url_path_display  : page.url_path_display.sub(toc_root_path,"").sub(/^\//,'')
+              "<li><a href=\"/#{page.url_path}\">#{path_display}</a></li>"
             end
-	  }.join + '</ul>'
+	  end
+	  result = "<ul>#{list_items.join}</ul>"
         end
         "<div class=\"toc\"><div class=\"toc-title\">#{title}</div>#{result}</div>"
       end
