123bca3f2f25c1d1c7401b55f0a3941af7c89172
rgroux <richard.groux+github@gmail.com>
2015-12-03 12:46:33 +0100

    feature(macro): navigation - set root-at-current-page
    as requested in PR #163 the default is root at current page

--- lib/gollum-lib/macro/navigation.rb
+++ lib/gollum-lib/macro/navigation.rb
@@ -1,11 +1,12 @@
 module Gollum
   class Macro
     class Navigation < Gollum::Macro
-      def render(title = "Navigate in the TOC", toc_root_path = "")
+      def render(title = "Navigate in the TOC", toc_root_path = File.dirname(@page.path), full_path = false)
         if @wiki.pages.size > 0
           result = '<ul>' + @wiki.pages.map {|p|
-            if p.url_path.match(toc_root_path)
-              "<li><a href=\"#{p.url_path}\">#{p.url_path_display}</a></li>"
+            if p.url_path.start_with?(toc_root_path)
+	      path_display = full_path ? p.url_path_display  : p.url_path_display.sub(toc_root_path,"").sub(/^\//,'')
+              "<li><a href=\"/#{p.url_path}\">#{path_display}</a></li>"
             end
 	  }.join + '</ul>'
         end
