fe9b32b0127daef2389091a53606ef5f4eeb6a8c
Victor Bogado <victor@bogado.net>
2016-12-30 16:51:21 -0800

    Navigation macro: Render current page differently.
    The current page needs to be highlighted and there's no need for a link
    to "here".

--- lib/gollum-lib/macro/navigation.rb
+++ lib/gollum-lib/macro/navigation.rb
@@ -2,12 +2,22 @@ module Gollum
   class Macro
     class Navigation < Gollum::Macro
 
-      def render(title = "Navigate in the TOC", toc_root_path = ::File.dirname(@page.sub_page ? @page.parent_page.path : @page.path), full_path = false)
+      def render(title = "Navigate in the TOC", toc_root_path = "", full_path = false)
+        my_page = @page.sub_page ? @page.parent_page : @page;
+
+        if toc_root_path == "" then
+          toc_root_path = ::File.dirname(my_page.path)
+        end
+
         if @wiki.pages.size > 0
           list_items = @wiki.pages.map do |page|
             if page.url_path.start_with?(toc_root_path)
               path_display = full_path ? page.url_path_display  : page.url_path_display.sub(toc_root_path,"").sub(/^\//,'')
-              "<li><a href=\"/#{page.url_path}\">#{path_display}</a></li>"
+              if page.path != my_page.path then
+                  "<li><a href=\"/#{page.url_path}\">#{path_display}</a></li>"
+              else
+                  "<li class=\"current_page\">#{path_display}</li>"
+              end
             end
           end
           result = "<ul>#{list_items.join}</ul>"
