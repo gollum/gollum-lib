d76583ebacd53601ffdfa0ce3234b2a0aee7daa5
Victor Bogado <victor@bogado.net>
2016-12-30 17:19:11 -0800

    Navigation macro: allow to add the directory name on title.
    The directory name can add some context to the navigation.
    
    The name is placed where the placeholder "%%" is located.

--- lib/gollum-lib/macro/navigation.rb
+++ lib/gollum-lib/macro/navigation.rb
@@ -2,6 +2,10 @@ module Gollum
   class Macro
     class Navigation < Gollum::Macro
 
+      def format(name)
+        name.sub("_", " ")
+      end
+
       def render(title = "Navigate in the TOC", toc_root_path = "", full_path = false)
         my_page = @page.sub_page ? @page.parent_page : @page;
 
@@ -9,10 +13,16 @@ module Gollum
           toc_root_path = ::File.dirname(my_page.path)
         end
 
+        title.sub! "%%" do
+            format(::File.basename(toc_root_path))
+        end
+
         if @wiki.pages.size > 0
           list_items = @wiki.pages.map do |page|
             if page.url_path.start_with?(toc_root_path)
               path_display = full_path ? page.url_path_display  : page.url_path_display.sub(toc_root_path,"").sub(/^\//,'')
+              path_display = format(path_display)
+
               if page.path != my_page.path then
                   "<li><a href=\"#{::File.join(page.wiki.base_path, page.url_path)}\">#{path_display}</a></li>"
               else
