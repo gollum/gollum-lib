53f36140ba010af140f2c9c866083815134033ce
Dawa Ometto <dawa.ometto@phil.uu.nl>
2017-04-04 13:50:01 +0200

    Update gemojione

--- gemspec.rb
+++ gemspec.rb
@@ -30,7 +30,7 @@ def specification(version, default_adapter, platform = nil)
     s.add_dependency 'stringex', '~> 2.6'
     s.add_dependency 'sanitize', '~> 2.1'
     s.add_dependency 'github-markup', '~> 1.6'
-    s.add_dependency 'gemojione', '~> 2.2'
+    s.add_dependency 'gemojione', '~> 3.2'
 
     s.add_development_dependency 'org-ruby', '~> 0.9.9'
     s.add_development_dependency 'kramdown', '~> 1.6.0'
