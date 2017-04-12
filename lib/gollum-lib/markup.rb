6c236a4f0443214c9d7a6824295fa5ce95f3443d
Dawa Ometto <dawa.ometto@phil.uu.nl>
2017-04-12 20:46:52 +0200

    Display a warning when attempting to use depracated :regexp option in Markup#register

--- lib/gollum-lib/markup.rb
+++ lib/gollum-lib/markup.rb
@@ -41,19 +41,24 @@ module Gollum
       # ext     - The file extension
       # name    - The name of the markup type
       # options - Hash of options:
-      #           regexp - Regexp to match against.
-      #                    Defaults to exact match of ext.
+      #           extensions - Array of valid file extensions, for instance ['md']
       #           enabled - Whether the markup is enabled
       #
       # If given a block, that block will be registered with GitHub::Markup to
       # render any matching pages
       def register(ext, name, options = {}, &block)
+        if options[:regexp] then
+          STDERR.puts <<-EOS
+          Warning: attempted to register a markup (name: #{name.to_s}) by passing the deprecated :regexp option.
+          Please pass an Array of valid file extensions (:extensions => ['ext1', 'ext2']) instead.
+          EOS
+        end
         new_extension = options.fetch(:extensions, [ext.to_s])
         @formats[ext] = { :name => name,
           :extensions => new_extension,
           :reverse_links => options.fetch(:reverse_links, false),
           :enabled => options.fetch(:enabled, true) }
-        @extensions.concat new_extension
+        @extensions.concat(new_extension)
       end
     end
 
