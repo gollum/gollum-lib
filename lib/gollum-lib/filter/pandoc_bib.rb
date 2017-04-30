2b385e11a2fa97275b94f7741a02c97b0054e848
Dawa Ometto <dawa.ometto@phil.uu.nl>
2017-04-30 13:11:10 +0200

    Slight refactoring. Implement #supported_format? so users can more easily override.

--- lib/gollum-lib/filter/pandoc_bib.rb
+++ lib/gollum-lib/filter/pandoc_bib.rb
@@ -1,6 +1,7 @@
 require 'yaml'
 require 'pathname'
 
+# When using pandoc, put relevant bibliography metadata extracted in the YAML filter back in the document so it gets passed on to pandoc.
 class Gollum::Filter::PandocBib < Gollum::Filter
 
   BIB_KEYS = ['bibliography', 'csl']
@@ -10,17 +11,18 @@ class Gollum::Filter::PandocBib < Gollum::Filter
   end
 
   def extract(data)
-    return data unless @markup.format == :markdown && @markup.metadata && bibliography_metadata_present? && using_pandoc?
+    return data unless supported_format? && bibliography_metadata_present? && using_pandoc?
     bib_metadata = {}
 
     BIB_KEYS.each do |bibliography_key|
-      path = @markup.metadata[bibliography_key]
-      next unless file = @markup.wiki.file(path)
-      path = Pathname.new("#{::File.join(::Dir.tmpdir, file.sha)}#{::File.extname(path)}")
-      bib_metadata[bibliography_key] = path.to_s
-      unless path.exist?
-        path.open('w') do |copy_file|
-          copy_file.write file.raw_data
+      if path = @markup.metadata[bibliography_key]
+        next unless file = @markup.wiki.file(path)
+        path = Pathname.new("#{::File.join(::Dir.tmpdir, file.sha)}#{::File.extname(path)}")
+        bib_metadata[bibliography_key] = path.to_s
+        unless path.exist?
+          path.open('w') do |copy_file|
+            copy_file.write(file.raw_data)
+          end
         end
       end
     end
@@ -29,11 +31,15 @@ class Gollum::Filter::PandocBib < Gollum::Filter
 
   private
 
+  def supported_format?
+    @markup.format == :markdown
+  end
+
   def using_pandoc?
     GitHub::Markup::Markdown.new.implementation_name == 'pandoc-ruby'
   end
 
   def bibliography_metadata_present?
-    @markup.metadata.keys.any? {|key| BIB_KEYS.include?(key)}
+     @markup.metadata && @markup.metadata.keys.any? {|key| BIB_KEYS.include?(key)}
   end
 end
