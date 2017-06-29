ab91eb9e4ef4bd9a0973e2f0df6e2c3276d6411e
jsieh <joel.sieh@gmail.com>
2017-06-29 16:04:32 -0400

    Fixed issue with GlobalTOC() not working with a base_path other than / . This was issue #280 on the main gollum-lib project.

--- lib/gollum-lib/macro/global_toc.rb
+++ lib/gollum-lib/macro/global_toc.rb
@@ -3,7 +3,9 @@ module Gollum
     class GlobalTOC < Gollum::Macro
       def render(title = "Global Table of Contents")
         if @wiki.pages.size > 0
-          result = '<ul>' + @wiki.pages.map { |p| "<li><a href=\"/#{p.url_path}\">#{p.url_path_display}</a></li>" }.join + '</ul>'
+          # Strip trailing slash.  This is important if base_path is /
+          prepath=@wiki.base_path.sub(/\/$/, '')
+          result = '<ul>' + @wiki.pages.map { |p| "<li><a href=\"#{prepath}/#{p.url_path}\">#{p.url_path_display}</a></li>" }.join + '</ul>'
         end
         "<div class=\"toc\"><div class=\"toc-title\">#{title}</div>#{result}</div>"
       end
