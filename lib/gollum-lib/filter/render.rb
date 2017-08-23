e26e841c5b1175f13cac0e2b6b35eb3e99d7368c
David Yates <davidmwyates@gmail.com>
2017-08-23 22:58:46 +0200

    Determine markup format from format name rather than filename

--- lib/gollum-lib/filter/render.rb
+++ lib/gollum-lib/filter/render.rb
@@ -3,7 +3,12 @@
 class Gollum::Filter::Render < Gollum::Filter
   def extract(data)
     begin
-      data = GitHub::Markup.render(@markup.name, data)
+      format = Gollum::Markup.formats[@markup.format][:name]
+      format = "rest" if format == "reStructuredText"
+      format = "org" if format == "Org-mode"
+
+      # format is determined from file extension
+      data = GitHub::Markup.render("."+format.downcase, data)
       if data.nil?
         raise "There was an error converting #{@markup.name} to HTML."
       end
