405c6bba1440a632c3ec44cce92a6b8777fbce71
Karl N. Redman <karl.redman@gmail.com>
2017-08-27 23:52:11 -0500

    better config, adding notes, adding timetrap (initial)

--- test/test_page.rb
+++ test/test_page.rb
@@ -256,11 +256,12 @@ context "within a sub-directory" do
   test "should inherit header/footer/sidebar pages from parent directories" do
     page = @wiki.page('Elrond')
 
+    assert page.sidebar.parent_page == page
+
     assert_equal Gollum::Page, page.sidebar.class
     assert_equal Gollum::Page, page.header.class
     assert_equal Gollum::Page, page.footer.class
 
-    assert page.sidebar.parent_page = page
     assert page.sidebar.raw_data =~ /^Lord of the Rings/
     assert page.header.raw_data =~ /^Hobbits/
     assert page.footer.raw_data =~ /^Lord of the Rings/
