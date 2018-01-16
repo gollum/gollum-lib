6a04e8f6b26769d326a5564ab4ae0308981f262f
Bart Kamphorst <bart@kamphorst.com>
2018-01-16 12:59:09 +0100

    Modify require path in test file.

--- test/filter/test_plantuml.rb
+++ test/filter/test_plantuml.rb
@@ -1,5 +1,5 @@
 # ~*~ encoding: utf-8 ~*~
-require File.expand_path(File.join(File.dirname(__FILE__), "helper"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "helper"))
 
 context "Page" do
 
