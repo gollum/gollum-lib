9749d1281ad4846c13a93e38585ae476bc29022c
Dawa Ometto <d.ometto@gmail.com>
2018-10-14 15:18:24 +0200

    Prepend base_path to links in Navigation() (#304)
    * Prepend base_path to links in Navigation()
    
    * Improve Navigation() path checking
    
    * Add test for Navigation Macro

--- lib/gollum-lib/macro/navigation.rb
+++ lib/gollum-lib/macro/navigation.rb
@@ -4,10 +4,11 @@ module Gollum
 
       def render(title = "Navigate in the TOC", toc_root_path = ::File.dirname(@page.path), full_path = false)
         if @wiki.pages.size > 0
+          prepath=@wiki.base_path.sub(/\/$/, '')
           list_items = @wiki.pages.map do |page|
-            if page.url_path.start_with?(toc_root_path)
-              path_display = full_path ? page.url_path_display  : page.url_path.sub(toc_root_path,"").sub(/^\//,'')
-              "<li><a href=\"/#{page.url_path}\">#{path_display}</a></li>"
+            if toc_root_path == '.' || page.url_path =~ /^#{toc_root_path}\//
+              path_display = (full_path || toc_root_path == '.') ? page.url_path  : page.url_path.sub(/^#{toc_root_path}\//,"").sub(/^\//,'')
+              "<li><a href=\"#{prepath}/#{page.escaped_url_path}\">#{path_display}</a></li>"
             end
           end
           result = "<ul>#{list_items.join}</ul>"
