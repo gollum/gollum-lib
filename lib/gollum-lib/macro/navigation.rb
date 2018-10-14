a2346a85e28fa397f1c01598fec7b89ae1365775
Dawa Ometto <dawa.ometto@phil.uu.nl>
2018-10-14 14:55:55 +0200

    Improve Navigation() path checking

--- lib/gollum-lib/macro/navigation.rb
+++ lib/gollum-lib/macro/navigation.rb
@@ -6,9 +6,9 @@ module Gollum
         if @wiki.pages.size > 0
           prepath=@wiki.base_path.sub(/\/$/, '')
           list_items = @wiki.pages.map do |page|
-            if page.url_path.start_with?(toc_root_path)
-              path_display = full_path ? page.url_path  : page.url_path.sub(toc_root_path,"").sub(/^\//,'')
-              "<li><a href=\"#{prepath}/#{page.url_path_escaped}\">#{path_display}</a></li>"
+            if toc_root_path == '.' || page.url_path =~ /^#{toc_root_path}\//
+              path_display = (full_path || toc_root_path == '.') ? page.url_path  : page.url_path.sub(/^#{toc_root_path}\//,"").sub(/^\//,'')
+              "<li><a href=\"#{prepath}/#{page.escaped_url_path}\">#{path_display}</a></li>"
             end
           end
           result = "<ul>#{list_items.join}</ul>"
