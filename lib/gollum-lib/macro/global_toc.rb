469b697bc64e12221b176a53ec6b2bb093eab66c
Dawa Ometto <d.ometto@gmail.com>
2018-10-14 13:20:18 +0200

    Update global_toc.rb

--- lib/gollum-lib/macro/global_toc.rb
+++ lib/gollum-lib/macro/global_toc.rb
@@ -4,7 +4,7 @@ module Gollum
       def render(title = "Global Table of Contents")
         if @wiki.pages.size > 0
           prepath=@wiki.base_path.sub(/\/$/, '')
-          result = '<ul>' + @wiki.pages.map { |p| "<li><a href=\"#{prepath}/#{p.url_path}\">#{p.url_path}</a></li>" }.join + '</ul>'
+          result = '<ul>' + @wiki.pages.map { |p| "<li><a href=\"#{prepath}/#{p.escaped_url_path}\">#{p.url_path}</a></li>" }.join + '</ul>'
         end
         "<div class=\"toc\"><div class=\"toc-title\">#{title}</div>#{result}</div>"
       end
