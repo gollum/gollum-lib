ef70522a60ade616b68cabcfbce050622d81785b
Dawa Ometto <dawa.ometto@phil.uu.nl>
2018-10-19 20:56:46 +0200

    Fixup: store merged global data in page's metadata

--- lib/gollum-lib/page.rb
+++ lib/gollum-lib/page.rb
@@ -225,8 +225,7 @@ module Gollum
     def metadata
       unless @metadata
         formatted_data if markup.metadata == nil
-        @metadata = markup.metadata || {}
-        @metadata.merge(@wiki.metadata)
+        @metadata = (markup.metadata || {}).merge(@wiki.metadata)
       else
         @metadata
       end
