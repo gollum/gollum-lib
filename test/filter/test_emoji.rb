d14ab5ced5c35de3e35bcd77eec4808dd8fc84e8
Dawa Ometto <dawa.ometto@phil.uu.nl>
2018-12-28 23:13:25 +0100

    Update emoji tests

--- test/filter/test_emoji.rb
+++ test/filter/test_emoji.rb
@@ -12,8 +12,8 @@ context "Gollum::Filter::Emoji" do
   end
 
   test "processing emoji tags" do
-    assert_equal filter(':heart:'), %q(<img src="/emoji/heart" alt="heart" class="emoji">)
-    assert_equal filter(':point_up_tone3:'), %q(<img src="/emoji/point_up_tone3" alt="point_up_tone3" class="emoji">)
+    assert_equal filter(':heart:'), %q(<img src="/gollum/emoji/heart" alt="heart" class="emoji">)
+    assert_equal filter(':point_up_tone3:'), %q(<img src="/gollum/emoji/point_up_tone3" alt="point_up_tone3" class="emoji">)
     assert_equal filter(':oggy_was_here:'), ':oggy_was_here:'
     assert_equal filter('rake app\:shell:install'), 'rake app:shell:install'
   end
