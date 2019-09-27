0c097c39a23c7a1e0bceca74e6fe1ac612b412eb
Bart Kamphorst <bart@kamphorst.com>
2019-09-27 14:27:04 +0200

    Extend Note() macro with optional arg for changing the octicon.

--- lib/gollum-lib/macro/note.rb
+++ lib/gollum-lib/macro/note.rb
@@ -1,10 +1,18 @@
 module Gollum
   class Macro
     class Note < Gollum::Macro
-      def render(notice)
-        icon = Octicons::Octicon.new('info', {width: 24, height: 24})
-        icon.options[:class] << ' mr-2'
-        "<div class='flash'>#{icon.to_svg}#{notice}</div>"
+      def render(notice, octicon = 'info')
+        icon = ""
+        unless octicon.empty?
+          begin
+            icon = Octicons::Octicon.new(octicon, {width: 24, height: 24})
+          rescue RuntimeError
+            icon = Octicons::Octicon.new('info', {width: 24, height: 24})
+          end
+          icon.options[:class] << ' mr-2'
+          icon = icon.to_svg
+        end
+        "<div class='flash'>#{icon}#{notice}</div>"
       end
     end
   end
