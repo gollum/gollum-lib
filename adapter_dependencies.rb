cd0394c6d29d0027ea74fa46b35134455ee8b05f
Dawa Ometto <dawa.ometto@phil.uu.nl>
2020-03-16 12:34:58 +0100

    Add explanation

--- adapter_dependencies.rb
+++ adapter_dependencies.rb
@@ -1,3 +1,5 @@
+# Set the default git adapter for use in gollum-lib.gemspec and gollum-lib_java.gemspec
+
 if RUBY_PLATFORM == 'java' then
   DEFAULT_ADAPTER_REQ = ['gollum-rjgit_adapter', '>= 0.5.1', '~> 0.5.1']
 else
