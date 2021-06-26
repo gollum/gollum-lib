ec7d3caf7de9c4f2c6b4283744bb2df7fbe65f32
Dawa Ometto <d.ometto@gmail.com>
2021-06-26 12:27:32 +0200

    Update gemspec.rb (#401)

--- gemspec.rb
+++ gemspec.rb
@@ -29,7 +29,7 @@ def specification(version, default_adapter, platform = nil)
     s.add_dependency 'rouge', '~> 3.1'
     s.add_dependency 'nokogiri', '~> 1.8'
     s.add_dependency 'loofah', '~> 2.3'
-    s.add_dependency 'github-markup', '~> 3.0'
+    s.add_dependency 'github-markup', '~> 4.0'
     s.add_dependency 'gemojione', '~> 4.1'
     s.add_dependency 'octicons', '~> 12.0'
     s.add_dependency 'twitter-text', '1.14.7'
