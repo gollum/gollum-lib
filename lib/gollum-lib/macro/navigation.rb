5801cb3a99951c23120531fd556630de646aab75
yy0931 <54441600+yy0931@users.noreply.github.com>
2021-07-10 23:19:44 +0900

    sanitize macros (#402)
    * Escape insecure HTML in macros

--- lib/gollum-lib/macro/navigation.rb
+++ lib/gollum-lib/macro/navigation.rb
@@ -9,12 +9,12 @@ module Gollum
           list_items = pages.map do |page|
             if toc_root_path == '.' || page.url_path =~ /^#{toc_root_path}\//
               path_display = (full_path || toc_root_path == '.') ? page.url_path  : page.url_path.sub(/^#{toc_root_path}\//,"").sub(/^\//,'')
-              "<li><a href=\"#{prepath}/#{page.escaped_url_path}\">#{path_display}</a></li>"
+              "<li><a href=\"#{CGI::escapeHTML(prepath + "/" + page.escaped_url_path)}\">#{CGI::escapeHTML(path_display)}</a></li>"
             end
           end
           result = "<ul>#{list_items.join}</ul>"
         end
-        "<div class=\"toc\"><div class=\"toc-title\">#{title}</div>#{result}</div>"
+        "<div class=\"toc\"><div class=\"toc-title\">#{CGI::escapeHTML(title)}</div>#{result}</div>"
       end
 
     end
--- lib/gollum-lib/macro/video.rb
+++ lib/gollum-lib/macro/video.rb
@@ -2,7 +2,7 @@ module Gollum
   class Macro
     class Video < Gollum::Macro
       def render (fname)
-        "<video width=\"100%\" height=\"100%\" src=\"#{fname}\" controls=\"\"> HTML5 video is not supported on this Browser.</video>"
+        "<video width=\"100%\" height=\"100%\" src=\"#{CGI::escapeHTML(fname)}\" controls=\"\"> HTML5 video is not supported on this Browser.</video>"
       end
     end
   end
