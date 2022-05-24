86d00203bf295b3e5b82823615eda2c46e28dc9a
Dawa Ometto <d.ometto@gmail.com>
2022-05-24 16:39:39 +0200

    Update octicons

--- gemspec.rb
+++ gemspec.rb
@@ -26,7 +26,7 @@ def specification(version, default_adapter, platform = nil)
     s.add_dependency 'loofah', '~> 2.3'
     s.add_dependency 'github-markup', '~> 4.0'
     s.add_dependency 'gemojione', '~> 4.1'
-    s.add_dependency 'octicons', '~> 12.0'
+    s.add_dependency 'octicons', '~> 17.0'
     s.add_dependency 'twitter-text', '1.14.7'
 
     s.add_development_dependency 'org-ruby', '~> 0.9.9'
