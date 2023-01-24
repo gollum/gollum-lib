0e475b849b15450ac6026989a5632f980b9c50f4
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2023-01-24 22:06:17 +0100

    Refactor helper method

--- lib/gollum-lib/helpers.rb
+++ lib/gollum-lib/helpers.rb
@@ -3,11 +3,12 @@
 module Gollum
   module Helpers
     
-    # If url starts with a leading slash, trim down its number of leading slashes to 1. Else, return url unchanged.
+    # Replace url-encoded slashes ('%2F') with slashes
+    # Clean up double slashes
     def trim_leading_slashes(url)
       return nil if url.nil?
       url.gsub!('%2F', '/')
-      (Pathname.new('/') + url).cleanpath.to_s
+      Pathname.new(url).cleanpath.to_s
     end
     
     # Take a link path and turn it into a string for display as link text.
