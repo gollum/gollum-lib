0facdce8a97bf29571908ffc9eee8ef1ce9f3561
Bart Kamphorst <bart@kamphorst.com>
2023-03-12 14:31:44 +0100

    Improve fix for setting flash type in Flash macro.

--- lib/gollum-lib/macro/flash.rb
+++ lib/gollum-lib/macro/flash.rb
@@ -2,7 +2,7 @@ module Gollum
   class Macro
     class Flash < Gollum::Macro
       def render(message, type='')
-        flash_type = "flash-#{type}" if ['warn', 'error', 'success'].include?(type)
+        flash_type = ['warn', 'error', 'success'].include?(type) ? "flash-#{type}" : '' 
         %Q(<div class="flash #{flash_type} my-2">#{message}</div>)
       end
     end
