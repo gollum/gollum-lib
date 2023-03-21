1f47c03a97ef0034d59a039fbdb4490e73e3f377
Alexander Ljungberg <aljungberg@wireload.net>
2023-03-21 13:21:32 +0000

    `<<Video ..., auto=true>>` to auto-play inline video (GIF style) (#442)
    * New: `<<Video ..., auto=true>>` plays clips inline on a loop (GIF style).
    - Using the `="true"` work-around as in #430.
    - The change in `sanitization.rb` further addresses gollum/gollum#1587 by allowing more of the standard video properties.

--- lib/gollum-lib/macro/video.rb
+++ lib/gollum-lib/macro/video.rb
@@ -1,8 +1,10 @@
 module Gollum
   class Macro
     class Video < Gollum::Macro
-      def render (fname)
-        "<video width=\"100%\" height=\"100%\" src=\"#{CGI::escapeHTML(fname)}\" controls=\"true\"> HTML5 video is not supported on this Browser.</video>"
+      def render(fname, auto=false)
+        escaped_fname = CGI.escapeHTML(fname)
+        properties = auto ? "autoplay='true' playsinline='true' muted='true' loop='true'" : "controls='true'"
+        "<video width='100%' height='100%' src='#{escaped_fname}' #{properties}>HTML5 video is not supported on this browser.</video>"
       end
     end
   end
