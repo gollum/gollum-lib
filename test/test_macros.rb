3a0435a51cc14228812351092d22ff669afee7c5
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2023-03-26 20:09:00 +0200

    Ensure Macro boolean arguments are parsed as boolean.

--- test/test_macros.rb
+++ test/test_macros.rb
@@ -50,6 +50,13 @@ context "Macros" do
     assert_match /<div class="toc"><div class="toc-title">Navigate this directory<\/div><ul><li><a href="\/NavigationMacroPage.md" rel="nofollow">NavigationMacroPage.md<\/a><\/li><li><a href="\/ZZZZ\/A\.md" rel="nofollow">ZZZZ\/A\.md<\/a><\/li><\/ul><\/div>/, @wiki.pages[0].formatted_data
   end
 
+  test "Navigation macro does not show fullpath if last parameter is full_path is set to false" do
+    # This is also a regression test against https://github.com/gollum/gollum-lib/issues/446
+    @wiki.write_page("NavigationMacroPage", :markdown, '<<Navigation("My TOC", "ZZZZ", false)>>', commit_details)
+    @wiki.write_page("ZZZZ/A", :markdown, "content", commit_details)
+    assert_match /<div class="toc"><div class="toc-title">My TOC<\/div><ul><li><a href="\/ZZZZ\/A\.md" rel="nofollow">A\.md<\/a><\/li><\/ul><\/div>/, @wiki.pages[0].formatted_data
+  end
+
   test "Series macro displays series links with and without series prefix" do
     @wiki.write_page("test-series1", :markdown, "<<Series(test)>>", commit_details)
     testseries1 = @wiki.page("test-series1")
