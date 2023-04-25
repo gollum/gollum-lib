d8be6130c7e72a07469cca83a7be5949be4c1005
Bart Kamphorst <bart@kamphorst.com>
2023-04-25 20:49:11 +0200

    Add optional icon parameter to Flash macro. Fix JRuby test.

--- lib/gollum-lib/macro/flash.rb
+++ lib/gollum-lib/macro/flash.rb
@@ -1,9 +1,10 @@
 module Gollum
   class Macro
     class Flash < Gollum::Macro
-      def render(message, type='')
+      def render(message, icon='', type='')
         flash_type = ['warn', 'error', 'success'].include?(type) ? "flash-#{type}" : '' 
-        %Q(<div class="flash #{flash_type} my-2">#{message}</div>)
+        flash_icon = icon.empty? ? '' : %Q(data-gollum-icon="#{icon}")
+        %Q(<div class="flash #{flash_type} my-2" #{flash_icon}>#{message}</div>)
       end
     end
   end
